f32e39ca9251abcfe954dab1b3625523000c8eb3
Vin123 <79678068+ceen123@users.noreply.github.com>
2023-11-19 23:36:31 +0800

    Update hw4.py

--- homework/hw4.py
+++ homework/hw4.py
@@ -1,1 +1,9 @@
-print('hw4')
+import math
+from numpy import arange
+def equation(x):
+    return x**2 - 3*x + 1
+
+for i in arange(-100, 100,0.001):
+    fx = equation(i)
+    if abs(equation(i)) < 0.001:
+        print(f"x = {i}, f(x) = {fx}")
